e6d895a967ac22b54400481960c6f6d5e085b04d
Pryanya06 <foralice06@mail.ru>
2023-07-15 10:12:50 +0300

    HW9

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
 public class Main {
+
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        System.out.println(" Задача 1");
+        int[] arr = generateRandomArray();
+    }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+    }
 
+    public static void task3() {
+        System.out.println("Задача 3");
+    }
 
-
+    public static void task4(){
+        System.out.println("Задача 4");
     }
 }
